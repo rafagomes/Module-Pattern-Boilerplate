79bc688526b4613373d363155a18023f071c7b19
Rafael Gomes <rafaelgomes.web@gmail.com>
2013-03-05 16:53:00 -0300

    Changing and validating with JSLINT

--- module.pattern.boilerplate.js
+++ module.pattern.boilerplate.js
@@ -1,41 +1,34 @@
-window.MAINNAMESPACE = window.MAINNAMESPACE || {};
-MAINNAMESPACE.SUBNAMESPACE = MAINNAMESPACE.SUBNAMESPACEUI || {}; //you can put something like PROJECT.UTIL
+/*global window, document, jQuery, namespace, mainnamespace, subnamespace*/
+window.namespace = window.namespace || {};
+mainnamespace.subnamespace = mainnamespace.subnamespace || {}; //you can put something like PROJECT.UTIL
 
-/*
- In this example I'm protecting the $ of Jquery, preventing conflicts.
-*/
+(function (window, document, $) {
 
+    "use strict";
 
-(function($){
-    
-    MAINNAMESPACE.SUBNAMESPACE.Module = function(){
-        var privateObject; // all objects in this scope are private
-        
-        var privateMethod = function() { //its better use anonymous methods
-            //Here its the method's implementation
-        };
-        
+    mainnamespace.subnamespace.Module = function () {
+        var privateObject, // all objects in this scope are private
+
+            privateMethod = function () { //its better use anonymous methods
+                //Here its the method's implementation
+            };
+
         return { //all objects inside return are public
-            
-            publicMethod : function(){
+
+            publicMethod : function () {
                 //Implementation of Public Method
             },
-            
+
             // It's very usual implementing initialization's methods and jqueryBinds for bindObjects like $('object').bind('mouseEvent');
-            init: function(){ 
+            init: function () {
                 //Implementation of Initialization Method
             }
         };
     };
-    
-    $(function(){ //an example of encapsulated instantialization of a Module
-        var module = new MAINNAMESPACE.SUBNAMESPACE.Module();
+
+    $(function () { //an example of encapsulated instantialization of a Module
+        var module = new mainnamespace.subnamespace.Module();
         module.init();
     });
-    
-    $(function(){ //an example of static instantialization of a Module
-        MAINNAMESPACE.SUBNAMESPACE.StaticModule = new MAINNAMESPACE.SUBNAMESPACE.Module();
-        MAINNAMESPACE.SUBNAMESPACE.StaticModule.init();
-    });
-    
-}(jQuery));
+
+}(window, document, jQuery));
