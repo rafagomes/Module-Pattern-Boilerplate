59ecaf82aa7ac5dd2b971182fd1cdd4c93e33edc
Rafael Gomes <rafaelgomes.web@gmail.com>
2014-08-03 22:29:44 -0300

    Update module.pattern.boilerplate.js

--- module.pattern.boilerplate.js
+++ module.pattern.boilerplate.js
@@ -1,33 +1,32 @@
-/*global window, document, jQuery, namespace, mainnamespace*/
+/*global window, document, jQuery, namespace, subnamespace*/
 window.namespace = window.namespace || {};
-mainnamespace.subnamespace = mainnamespace.subnamespace || {}; //you can put something like PROJECT.UTIL
+namespace.subnamespace = namespace.subnamespace || {};
 
 (function (window, document, $) {
 
     "use strict";
 
-    mainnamespace.subnamespace.Module = function () {
-        var privateObject, // all objects in this scope are private
+    namespace.subnamespace.Module = function () {
+        var privateObject,
 
-            privateMethod = function () { //its better use anonymous methods
-                //Here its the method's implementation
+            privateMethod = function () {
+                
             };
 
-        return { //all objects inside return are public
+        return {
 
             publicMethod : function () {
-                //Implementation of Public Method
+                
             },
 
-            // It's very usual implementing initialization's methods and jqueryBinds for bindObjects like $('object').bind('mouseEvent');
             init: function () {
-                //Implementation of Initialization Method
+                
             }
         };
     };
 
-    $(function () { //an example of encapsulated instantialization of a Module
-        var module = new mainnamespace.subnamespace.Module();
+    $(function () {
+        var module = new namespace.subnamespace.Module();
         module.init();
     });
 
